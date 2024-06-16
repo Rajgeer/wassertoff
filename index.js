cd49eb9aa95cbbad436b49a7cb066bf43bda7605
unknown <rajgeerpaswan123@gmail.com>
2024-06-16 23:29:48 +0530

    update index file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 const router = require('./src/routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI|| 'mongodb+srv://devzonesoftech:D34XI23AZyQpbwG2@wasserstoff.wxajwyu.mongodb.net/';
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
@@ -14,7 +14,7 @@ app.use('/apis', router);
 mongoose
   .connect(MONGO_URI, {
     bufferCommands: true,
-    dbName: "wassertoff",
+    dbName: "BackendTask",
     autoIndex: true,
     autoCreate: true,
   })
