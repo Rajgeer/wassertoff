946ae77ad7999840853038e8e4ee9edf90a72b98
unknown <rajgeerpaswan123@gmail.com>
2024-06-17 22:20:05 +0530

    add cors in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const router = require('./src/routes');
 const app = express();
 const PORT = process.env.PORT||5000;
@@ -9,6 +10,7 @@ const mongoURI = process.env.MONGO_URI;
 if (!mongoURI) {
   throw new Error('MONGO_URI is not defined in the environment variables');
 }
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 app.use('/apis', router);
